ee49bc33461e96571d0f67c73bf10c9add1111d2
Daphineatim <hopedaphine19@gmail.com>
2022-10-27 18:04:32 +0300

    Refactor the Calculator component

--- src/App.js
+++ src/App.js
@@ -2,19 +2,8 @@ import React from 'react';
 import './index.css';
 import Calculator from './components/Calculator';
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-
-  render() {
-    return (
-      <div>
-        <Calculator />
-      </div>
-    );
-  }
+function App() {
+  return <Calculator />;
 }
 
 export default App;
