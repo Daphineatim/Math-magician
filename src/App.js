d32a4e976c749d2638c02fd11ee40fc71595beae
Daphineatim <hopedaphine19@gmail.com>
2022-11-02 13:00:25 +0300

    Fix linters

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './index.css';
 import Calculator from './components/Calculator';
+import NavBar from './components/NavBar';
+import Home from './components/Home';
+import Quote from './components/Quote';
 
 function App() {
-  return <Calculator />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<NavBar />}>
+          <Route index element={<Home />} />
+          <Route path="calculator" element={<Calculator />} />
+          <Route path="quote" element={<Quote />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
